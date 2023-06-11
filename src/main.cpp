d71696f36d12379dd4cf9c2a670e00a5a34ec029
Diego <diego.santiago.cocodin@gmail.com>
2023-06-11 13:21:24 +0200

    - lectura tensión salida ok

--- src/main.cpp
+++ src/main.cpp
@@ -44,57 +44,76 @@
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-byte polla[8] = {0x04, 0x04, 0x0E, 0x15, 0x04, 0x0E, 0x0A, 0x1B};
+/* byte tipo[8] = {0x04, 0x04, 0x0E, 0x15, 0x04, 0x0E, 0x0A, 0x1B};
 byte smiley[8] = {0b00000, 0b00000, 0b01010, 0b00000, 0b00000, 0b10001, 0b01110, 0b00000};
-byte heart[8] = {0b00000, 0b01010, 0b11111, 0b11111, 0b11111, 0b01110, 0b00100, 0b00000};
+byte heart[8] = {0b00000, 0b01010, 0b11111, 0b11111, 0b11111, 0b01110, 0b00100, 0b00000}; */
+byte uno[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF};
+byte dos[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF};
+byte tres[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF};
+byte cuatro[8] = {0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF};
+byte cinco[8] = {0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+byte seis[8] = {0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+byte siete[8] = {0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+byte ocho[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
-int sensorPin = A0;
-int sensorValue = 0;
+int sensorV = A4;
 
-float toDecimal(int value)
+float toDecimalVol(int value)
 {
-  float dec = ((float)value * 50 / 1024.0);
+  float dec = ((float)value * 5.0 / 1024.0) * 10;
   return dec;
 }
 
 void setup()
 {
-  // set up the LCD's number of columns and rows:
+  // ************** LCD **************
   lcd.begin(16, 2);
-
-  lcd.createChar(0, polla);
-  lcd.createChar(1, smiley);
-  lcd.createChar(2, heart);
-  // Print a message to the LCD.
-  lcd.setCursor(0, 0);
-  lcd.print("I ");
-  lcd.write((byte)2);
-  lcd.print(" Jony ");
-  lcd.write((byte)1);
-  lcd.print(" ");
-  lcd.write((byte)0);
+  lcd.createChar(0, uno);
+  lcd.createChar(1, dos);
+  lcd.createChar(2, tres);
+  lcd.createChar(3, cuatro);
+  lcd.createChar(4, cinco);
+  lcd.createChar(5, seis);
+  lcd.createChar(6, siete);
+  lcd.createChar(7, ocho);
+  // ************** CAD **************
+  // 1.1V analogReference(INTERNAL);
 }
 
 void loop()
 {
-  sensorValue = analogRead(sensorPin);
-  /*   float value = ((int) (dec * 100)) / 100.0; */
-  float value = toDecimal(sensorValue);
+  // ********* voltaje *********
+  int valueV = analogRead(sensorV);
+  float volt = toDecimalVol(valueV);
+  // *********** LCD ***********
   lcd.setCursor(0, 1);
-  lcd.print("                ");
-  if (value < 10)
+  lcd.print("Parrino         ");
+  for (int i = 0; i < 8; i++)
   {
-    lcd.setCursor(10, 1);
-    lcd.print(value);
+    float segment = (3 * i) + 11.0;
+    if (volt < segment)
+    {
+      break;
+    }
+    lcd.setCursor(i + 8, 1);
+    lcd.write((byte)i);
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+  if (volt < 10)
+  {
+    lcd.setCursor(10, 0);
+    lcd.print(volt);
     lcd.print(" v");
   }
   else
   {
-    lcd.setCursor(9, 1);
-    lcd.print(value);
+    lcd.setCursor(9, 0);
+    lcd.print(volt);
     lcd.print(" v");
   }
 
